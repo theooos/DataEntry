9ccdf8cfcfbb87181f77e6a2668f5b3d67717ad7
theooos <tjmg@outlook.com>
2016-09-10 20:08:43 +0100

    Topics can be retrieved

--- script.js
+++ script.js
@@ -1,57 +1,88 @@
 
-var topicSelector = document.getElementById('topicSelector');
+var server = "http://54.171.145.89:3001/";
 
-var newQuestion = document.getElementById('newQuestion');
-var newQuestionStarter = document.getElementById('newQuestionStarter');
-var newQuestionTags = document.getElementById('newQuestionTags');
-var questionSelector = document.getElementById('questionSelector');
+window.onload = function () {
+    var topicSelector = document.getElementById('topicSelector');
 
-var newMessage = document.getElementById('newMessage');
-var newKeywords = document.getElementById('newKeywords');
-var newAcnol = document.getElementById('newAcnol');
-var newChangeTopic = document.getElementById('newChangeTopic');
-var newOurResponse = document.getElementById('newOurResponse');
+    var newQuestion = document.getElementById('newQuestion');
+    var newQuestionStarter = document.getElementById('newQuestionStarter');
+    var newQuestionTags = document.getElementById('newQuestionTags');
+    var questionSelector = document.getElementById('questionSelector');
+
+    var newMessage = document.getElementById('newMessage');
+    var newKeywords = document.getElementById('newKeywords');
+    var newAcnol = document.getElementById('newAcnol');
+    var newChangeTopic = document.getElementById('newChangeTopic');
+    var newOurResponse = document.getElementById('newOurResponse');
+};
 
 // ############ TOPICS ############
 // Clears topics dropdown, use addTopic to add one and topicChange is called when its changed
 function clearTopics() {
-    topicSelector.innerHTML = "";
-}
-
-function clearQuestions() {
-    questionSelector.innerHTML = "";
+    topicSelector.innerHTML = "<option value='' disabled selected>Select a topic</option>";
 }
 
 function addTopic(topicName) {
-    var option = document.createElement("option");
-    option.text = topicName;
-    topicSelector.add(option);
+    var topicJSON = {
+        "topic": topicName,
+        "questions":[]
+    };
+    $.post(server+"questions", topicJSON, function () {
+        alert("Successfully added "+topicName+".");
+        var option = document.createElement("option");
+        option.text = topicName;
+        topicSelector.add(option);
+    });
 }
 
 function loadTopics(){
-    $.get("http://54.171.145.89:80/topics", function(data){
-        alert("Data: " + data);
+    clearTopics();
+    $.get(server+"topics", function(topics){
+        topics.forEach(function (topic) {
+            var option = document.createElement("option");
+            option.text = topic;
+            topicSelector.add(option);
+        })
     });
 }
 
 
-
 // ############ QUESTIONS ###########
+// Clears questions dropdown
+function clearQuestions() {
+    questionSelector.innerHTML = "<option value='' disabled selected>Select a question</option>";
+}
+
+// Resets the question area to it's normal state.
+function resetQuestionArea(){
+    newQuestion.value = "";
+    newQuestionTags.value = "";
+}
 
 // When the question is added
-function questionAdded(questionName, questionStarter, questionTags) {
-    alert("Added: " + questionName + ", " + questionStarter + ", " + questionTags);
-    addQuestion(questionName);
-    newQuestion.value = "";
+function addQuestion(topic, questionName, questionStarter, questionTags) {
+    resetQuestionArea();
+    loadQuestions(topic);
 }
 
-// Adds thing to list
-function addQuestion(questionName) {
-    var option = document.createElement("option");
-    option.text = questionName;
-    questionSelector.add(option);
+// Populates questions dropdown with all questions for given topic.
+function loadQuestions(topic) {
+    console.log(server+"questions/"+topic);
+    $.get(server+"questions/"+topic, function(questions){
+        console.log("Questions: " + questions);
+        questions.forEach(function (question) {
+            var option = document.createElement("option");
+            option.text = topic;
+            questionSelector.add(option);
+        })
+    });
 }
 
+function questionChange(e) {
+    var question = e.value;
+    alert(question);
+    // This is where your code goes for
+}
 
 
 
@@ -64,16 +95,4 @@ function responseAdded(message, keywords, acnol, changetopic, ourresponse) {
     newAcnol.value = "";
     newChangeTopic.value = "";
     newOurResponse.value = "";
-}
-
-function topicChange(e) {
-    var topic = e.value;
-    alert(topic);
-    // This is where your code goes for
-}
-
-function questionChange(e) {
-    var question = e.value;
-    alert(question);
-    // This is where your code goes for
 }

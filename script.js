940712b0e16585eb837787ec9456cf0c23e3ded4
theooos <tjmg@outlook.com>
2016-09-10 23:29:40 +0100

    Finished, waiting to test with Aran's fixes.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var server = "http://54.171.145.89:3001/";
+var server = "http://138.68.139.139/";
 
 window.onload = function () {
     var topicSelector = document.getElementById('topicSelector');
@@ -11,13 +11,30 @@ window.onload = function () {
 
     var newMessage = document.getElementById('newMessage');
     var newKeywords = document.getElementById('newKeywords');
-    var newAcnol = document.getElementById('newAcnol');
+    var newReply = document.getElementById('newReply');
     var newChangeTopic = document.getElementById('newChangeTopic');
     var newOurResponse = document.getElementById('newOurResponse');
+
+
+    $("#newOurResponse").change(function(){
+        if($(this).val() == "t") {
+            newKeywords.value = "";
+            newKeywords.disabled = true;
+            newReply.value = "";
+            newReply.disabled = true;
+            newChangeTopic.value = "False";
+            newChangeTopic.disabled = true;
+
+        } else {
+            newKeywords.disabled = false;
+            newReply.disabled = false;
+            newChangeTopic.disabled = false;
+        }
+    });
 };
 
 // ############ TOPICS ############
-// Clears topics dropdown, use addTopic to add one and topicChange is called when its changed
+// Clears topics dropdown.
 function clearTopics() {
     topicSelector.innerHTML = "<option value='' disabled selected>Select a topic</option>";
 }
@@ -27,14 +44,12 @@ function addTopic(topicName) {
         "topic": topicName,
         "questions":[]
     };
-    $.post(server+"questions", topicJSON, function () {
-        alert("Successfully added "+topicName+".");
-        var option = document.createElement("option");
-        option.text = topicName;
-        topicSelector.add(option);
-    });
+    console.log(JSON.stringify(topicJSON));
+    send(topicJSON);
+    loadTopics();
 }
 
+// Populates topics dropdown.
 function loadTopics(){
     clearTopics();
     $.get(server+"topics", function(topics){
@@ -48,51 +63,103 @@ function loadTopics(){
 
 
 // ############ QUESTIONS ###########
-// Clears questions dropdown
-function clearQuestions() {
-    questionSelector.innerHTML = "<option value='' disabled selected>Select a question</option>";
-}
-
 // Resets the question area to it's normal state.
 function resetQuestionArea(){
+    questionSelector.innerHTML = "<option value='' disabled selected>Select a question</option>";
     newQuestion.value = "";
     newQuestionTags.value = "";
 }
 
 // When the question is added
-function addQuestion(topic, questionName, questionStarter, questionTags) {
-    resetQuestionArea();
+function addQuestion(topic, question, starter, tags) {
+    var questionJSON = {
+        "topic":topic,
+        "questions":[
+            {
+                "message": question,
+                "id":uuid(),
+                "isOpener":starter,
+                "presentKeywords":tags,
+                "children":[]
+            }
+
+        ]
+    }
+    console.log(JSON.stringify(questionJSON));
+    send(questionJSON);
     loadQuestions(topic);
 }
 
 // Populates questions dropdown with all questions for given topic.
 function loadQuestions(topic) {
-    console.log(server+"questions/"+topic);
-    $.get(server+"questions/"+topic, function(questions){
-        console.log("Questions: " + questions);
-        questions.forEach(function (question) {
+    resetQuestionArea();
+    $.get(server+"questions/"+topic, function(fullTopic){
+        var topicJSON = JSON.parse(JSON.stringify(fullTopic));
+        var questions = topicJSON["questions"];
+        questions.forEach(function(question){
             var option = document.createElement("option");
-            option.text = topic;
+            option.text = question["message"];
             questionSelector.add(option);
         })
     });
 }
 
-function questionChange(e) {
-    var question = e.value;
-    alert(question);
-    // This is where your code goes for
+
+// ########## RESPONSES ############
+function addResponse(topic, question, response, keywords, reply, changeTopic, ourResponse){
+    var responseJSON = {
+        "topic": topic,
+        "questions": [
+            {
+                "message": question,
+                "children":[
+                    {
+                        "message":response,
+                        "presentKeywords":keywords,
+                        "response":reply,
+                        "followUp":[],
+                        "switchTopic":changeTopic,
+                        "usedByAI":ourResponse
+                    }
+                ]
+            }
+        ]
+    }
+    send(responseJSON);
+    resetResponseArea();
+}
+
+function resetResponseArea() {
+    newMessage.value = "";
+    newKeywords.value = "";
+    newReply.value = "";
+    newChangeTopic.value = "";
+    newOurResponse.value = "";
 }
 
 
+// ######### OTHER ###########
+function uuid(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
 
-// ########## RESPONSES ############
+function send(data){
+    sendWithFunction(data, function (reply) {
+        console.log("[SENT LINE BELOW]");
+        console.log(data);
+        console.log("[SERVER] "+reply);
+    })
+}
 
-function responseAdded(message, keywords, acnol, changetopic, ourresponse) {
-    alert("Added: " + message + ", " + keywords + ", " + acnol + ", " + changetopic + ", " + ourresponse);
-    newMessage.value = "";
-    newKeywords.value = "";
-    newAcnol.value = "";
-    newChangeTopic.value = "";
-    newOurResponse.value = "";
+function sendWithFunction(data,func){
+    $.ajax({
+        type: "POST",
+        url: server+"questions",
+        data: JSON.stringify(data),
+        success: func,
+        contentType: 'application/json'
+    });
 }
